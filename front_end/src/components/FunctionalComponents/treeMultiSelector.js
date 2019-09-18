9905c4b1e7004a8aa1022ae06e5fceb3bd2f4f66
topdev10 <avantica777@gmail.com>
2019-09-17 21:36:07 -0400

    add new report filter and custom column selection dialog done

--- front_end/src/components/FunctionalComponents/treeMultiSelector.js
+++ front_end/src/components/FunctionalComponents/treeMultiSelector.js
@@ -15,20 +15,52 @@ const TreeContainer = styled.div`
     flex: 1;
     height: 100%;
     padding: 32px 12px 12px 12px;
+`;
+
+const TreeHeader = styled.div`
+    display: flex;
+    align-items: center;
+    background: #ccc;
+    color: black;
+    font-size: 18px;
+    height: 42px;
+    padding: 5px 10px;
+`;
+
+const TreeWrapper = styled.div`
+    display: flex;
+    height: 100%;
+    border: 2px solid #ccc;
     overflow: auto;
 `;
 
 const CancelButton = styled.div`
+    height: 40px;
     border-radius: 4px;
-    border: 2px solid #AFF;
+    justify-content: center;
+    align-items: center;
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 17px;
+    color: #FFFFFF;
+    background: #4D7CFE;
+    padding: 8px 12px;
+    flex: flex-end;
     cursor: pointer;
+    text-align: right;
+    margin: 8px 0px 0px 0px;
+
+    &:hover {
+        background: red;
+    }
 `;
 
 class TreeMultiSelector extends React.Component {
     state = {
         checked: [],
         expanded: ['all', 'container', 'shipment'],
-        clicked: {},
     };
 
     constructor(props) {
@@ -47,7 +79,22 @@ class TreeMultiSelector extends React.Component {
     }
 
     onClick(clicked) {
-        this.setState({ clicked });
+        const { checked } = this.state;
+        // eslint-disable-next-line react/prop-types
+        const { handleChecked } = this.props;
+        let flag = false;
+        let pos = 0;
+        checked.forEach((element, ind) => {
+            if(element === clicked.value)
+            {
+                flag = true;
+                pos = ind;
+            }
+        });
+        if(!flag)
+            checked.push(clicked.value);
+        else checked.splice(pos, 1);
+        handleChecked(checked);
     }
 
     onExpand(expanded) {
@@ -56,36 +103,41 @@ class TreeMultiSelector extends React.Component {
 
     render() {
 
-        const { checked, expanded, clicked } = this.state;
-        const notClickedText = '(none)';
+        const { checked, expanded } = this.state;
         // eslint-disable-next-line react/prop-types
         const { reportNodes, handleCancel } = this.props;
 
         return (
             <TreeContainer>
-                <CheckboxTree
-                    checked={checked}
-                    expanded={expanded}
-                    nodes={reportNodes}
-                    expandOnClick
-                    onCheck={this.onCheck}
-                    onClick={this.onClick}
-                    onExpand={this.onExpand}
-                    icons={{
-                        check: <img src={CheckboxChecked} alt="Chcked" />,
-                        uncheck:  <img src={CheckboxUnchcked} alt="UnChcked" />,
-                        halfCheck: <img src={CheckboxHalfChecked} alt="HalfChcked" />,
-                        expandClose: <img src={ExpandClose} alt="Expand" />,
-                        expandOpen: <img src={ExpandOpen} alt="Expand" />,
-                        expandAll: <img src={ExpandOpen} alt="Expand" />,
-                        collapseAll: <img src={ExpandOpen} alt="Expand" />,
-                        parentClose: "",
-                        parentOpen: "",
-                        leaf: "",
-                    }}
-                />
+                <TreeHeader>
+                    Choose columns for your report
+                </TreeHeader>
+                <TreeWrapper>
+                    <CheckboxTree
+                        checked={checked}
+                        expanded={expanded}
+                        nodes={reportNodes}
+                        expandOnClick
+                        onCheck={this.onCheck}
+                        onClick={this.onClick}
+                        onExpand={this.onExpand}
+                        icons={{
+                            check: <img src={CheckboxChecked} alt="Chcked" />,
+                            uncheck:  <img src={CheckboxUnchcked} alt="UnChcked" />,
+                            halfCheck: <img src={CheckboxHalfChecked} alt="HalfChcked" />,
+                            expandClose: <img src={ExpandClose} alt="Expand" />,
+                            expandOpen: <img src={ExpandOpen} alt="Expand" />,
+                            expandAll: <img src={ExpandOpen} alt="Expand" />,
+                            collapseAll: <img src={ExpandOpen} alt="Expand" />,
+                            parentClose: "",
+                            parentOpen: "",
+                            leaf: "",
+                        }}
+                        style={{border: "2px solid #ccc"}}
+                    />
+                </TreeWrapper>
                 <CancelButton onClick={handleCancel} >
-                    <strong>Clicked Node</strong>: {clicked.value || notClickedText}
+                    Cancel
                 </CancelButton>
             </TreeContainer>
         );
