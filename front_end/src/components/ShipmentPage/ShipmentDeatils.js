4a450fac0496fa5343f95a1448a4c3051f6f81fa
topdev10 <k100cool@oultook.com>
2020-01-04 20:18:05 +0800

    remove warnings in shipment details component

--- front_end/src/components/ShipmentPage/ShipmentDeatils.js
+++ front_end/src/components/ShipmentPage/ShipmentDeatils.js
@@ -7,7 +7,6 @@ import PropTyes from 'prop-types';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import PersonIcon from '@material-ui/icons/Person';
