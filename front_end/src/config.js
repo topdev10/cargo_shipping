9706bcad56f76cb88df421219fb3c90572704145
topdev10 <k100cool@outlook.com>
2020-03-19 23:30:08 -0700

    update config file

--- front_end/src/config.js
+++ front_end/src/config.js
@@ -7,18 +7,18 @@ class Config {
         // Local Url
         // this.BACKEND_API_URL = "http://localhost:8000/";
         this.PUSHER_APP_ID = "863077";
-        this.PUSHER_KEY = "2ea31d0b05f2da4caf6e";
-        this.PUSHER_SECRET = "7b5538b61ee06197ad3f";
+        this.PUSHER_KEY = "Your Pusher Key";
+        this.PUSHER_SECRET = "Your Pusher Secret";
         this.PUSHER_CLUSTER = "mt1";
         this.PUSHER_CHANNEL = "customer";
         // Google Map Api Key
         this.GOOGLE_PUBLIC_KEY = "Your Google Api Key";
-        this.LINKEDIN_CLIENT_ID_LIVE = "86w9dm8at7hfq5";
-        this.LINKEDIN_CLIENT_ID_LOCAL = "86rytomxqk2lfz";
+        this.LINKEDIN_CLIENT_ID_LIVE = "LinkedinClient_Live";
+        this.LINKEDIN_CLIENT_ID_LOCAL = "LinkedinLocal";
         this.LINKEDIN_REDIRECT_URL_LOCAL = "http%3A%2F%2Flocalhost%3A8000%2Fauth%2Flinkedin";
-        this.LINKEDIN_REDIRECT_URL_DEPLOY = "http%3A%2F%2Fec2-35-182-231-236.ca-central-1.compute.amazonaws.com%3A8000%2Fauth%2Flinkedin";
+        this.LINKEDIN_REDIRECT_URL_DEPLOY = "redirect url";
         // Stripe Payment
-        this.STRIPE_PUBLIC_KEY = "pk_test_mfCPqZtW4If6nVmSF2Ahv2xp0013VlcByR";
+        this.STRIPE_PUBLIC_KEY = "stripe key";
     }
 }
 
